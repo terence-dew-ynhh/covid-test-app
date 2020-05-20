a7c0209f4ebd81e7b35785dff7b79c76077758ed
TerenceJDew <terence.dew@gmail.com>
2020-05-20 16:49:07 -0400

    Updated Mychart Link

--- pages/scheduling.js
+++ pages/scheduling.js
@@ -14,7 +14,7 @@ export default function Home() {
         </img><span className="divider"></span>
         
       <div className="scheduleContainer">
-  <iframe id="openSchedulingFrame" className="widgetframe" scrolling="yes" src="https://mychart.ynhhs.org/MyChart-PRD/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=78061,78102&vt=2102&dept=101010165&view=plain&public=1"></iframe>
+  <iframe id="openSchedulingFrame" className="widgetframe" scrolling="yes" src="https://mychart.ynhhs.org/MyChart-PRD/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=78061,78102,78419&vt=2102&dept=101010165&view=plain&public=1"></iframe>
     </div>
       <style jsx>{`
         .scheduleContainer, iframe{

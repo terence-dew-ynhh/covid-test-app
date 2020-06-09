0cdbbf68e1fb286adb9c127807eea7aead31cadc
TerenceJDew <terence.dew@gmail.com>
2020-06-09 16:37:57 -0400

    Update to BH MC link

--- pages/scheduling.js
+++ pages/scheduling.js
@@ -51,7 +51,7 @@ Home.getInitialProps = async ({ query }) => {
     {
       name: 'Bridgeport Hospital - MC',
       link:
-        'https://mychart.ynhhs.org/MyChart-PRD/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=78405&vt=2102&dept=103700024&view=plain&public=1'
+        'https://mychart.ynhhs.org/MyChart-PRD/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=78405,78870&vt=2102&dept=103700024&view=plain&public=1'
     },
     {
       name: 'Greenwich Hospital',

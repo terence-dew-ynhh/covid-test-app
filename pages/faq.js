e305f9b26e9806758eadaeadc1bb56d6bd74d3ac
TerenceJDew <terence.dew@gmail.com>
2020-07-09 11:30:20 -0400

    loose imports fix

--- pages/faq.js
+++ pages/faq.js
@@ -1,13 +1,9 @@
 import React, { useState } from 'react';
 import Head from 'next/head';
-import Link from 'next/link';
-import QuestionFormComponent from '../components/QuestionFormComponent';
 
 export default function FAQ() {
   const [isCovidPositive, setIsCovidPositive] = useState('');
-  const [isTwoWeeksSince, setIsTwoWeeksSince] = useState('');
-
-  
+  const [isTwoWeeksSince, setIsTwoWeeksSince] = useState('');  
 
   return (
     <div className="container">

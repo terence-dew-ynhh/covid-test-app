e9608f46a10c8517643dfa8d70aa277e5f383644
TerenceJDew <terence.dew@gmail.com>
2020-07-17 16:18:03 -0400

    Port Update

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   publicRuntimeConfig: {
     NODE_ENV: process.env.NODE_ENV,
-    PORT: process.env.PORT || 3005
+    PORT: process.env.PORT || 3008
   },
   devIndicators: {
     autoPrerender: false,

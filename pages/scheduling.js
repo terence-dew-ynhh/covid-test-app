9ee2fc36cb1806bc89d16a38b0c083d4f8a1cf4a
TerenceJDew <terence.dew@gmail.com>
2020-07-20 09:30:44 -0400

    Link Removal Mohegan Sun

--- pages/scheduling.js
+++ pages/scheduling.js
@@ -12,8 +12,8 @@ export default function Home({ link }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className='grid'>
-      <img src="/YNHHSLogo.png"></img>
+      <div className="grid">
+        <img src="/YNHHSLogo.png"></img>
       </div>
 
       <div className="scheduleContainer">
@@ -52,37 +52,37 @@ Home.getInitialProps = async ({ query }) => {
       name: 'Connecticut Mental Health Center',
       link:
         'https://mychart.ynhhs.org/MyChart-PRD/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=79270&vt=2228&dept=100001338&view=plain&public=1'
-    },{
+    },
+    {
       name: 'Connecticut Valley Hospital',
       link:
         'https://mychart.ynhhs.org/MyChart-PRD/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=78420&vt=2228&dept=101010166&view=plain&public=1'
-    },{
+    },
+    {
       name: 'New Haven Judicial District',
       link:
         'https://mychart.ynhhs.org/MyChart-PRD/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=79325&vt=2228&dept=100001343&view=plain&public=1'
-    },{
+    },
+    {
       name: 'Bridgeport Judiciary',
       link:
         'https://mychart.ynhhs.org/MyChart-PRD/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=79272&vt=2228&dept=100001339&view=plain&public=1'
     },
     {
-      name: 'Mohegan Sun Employees',
-      link:
-        'https://mychart.ynhhs.org/MyChart-PRD/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=79643&vt=2228&dept=100001358&view=plain&public=1'
-    },{
       name: 'Capital Region Mental Health Center',
       link:
         'https://mychart.ynhhs.org/MyChart-PRD/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=79598&vt=2228&dept=100001355&view=plain&public=1'
-    },{
+    },
+    {
       name: 'Southeast Mental Health Authority',
       link:
         'https://mychart.ynhhs.org/MyChart-PRD/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=79599&vt=2228&dept=100001356&view=plain&public=1'
-    },{
+    },
+    {
       name: 'Western CT Mental Health Network',
       link:
         'https://mychart.ynhhs.org/MyChart-PRD/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=79600&vt=2228&dept=100001357&view=plain&public=1'
     }
-
   ];
 
   locationMapping.forEach((element) => {
@@ -95,4 +95,3 @@ Home.getInitialProps = async ({ query }) => {
     link
   };
 };
- 

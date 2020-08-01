5c129dfb989763f5fd4c80dd2b176a4cb435cccb
TerenceJDew <terence.dew@gmail.com>
2020-08-01 16:28:18 -0400

    Copy Update

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,24 @@
-import React, { useState } from "react";
-import Head from "next/head";
-import QuestionContainerComponent from "../components/QuestionContainerComponent";
+import React, { useState } from 'react';
+import Head from 'next/head';
+import QuestionContainerComponent from '../components/QuestionContainerComponent';
 
 export default function Home() {
- 
-
   return (
     <div className="container">
       <Head>
         <title>YNHH COVID-19 Test Scheduler</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className='grid'>
-      <img src="/YNHHSLogo.png"></img>
+      <div className="grid">
+        <img src="/YNHHSLogo.png"></img>
       </div>
       <h1 className="title">
-      An appointment is required to be tested; if there is not availability at your assigned location and preferred date, please search for an future date / time at your assigned location.  
-      If none are available at your location please check back.
-        </h1>
+        One time testing is available for state mandated individuals – ask your
+        supervisor if you have any questions. Do not sign up for asymptomatic
+        COVID testing if you have symptoms or have already been tested since
+        June 18th
+      </h1>
       <QuestionContainerComponent></QuestionContainerComponent>
-      
     </div>
   );
 }

ace35b93f7d0df558530193aefab5e9c785d2f52
TerenceJDew <terence.dew@gmail.com>
2020-09-29 11:20:05 -0400

    update logic (unfinished)

--- components/QuestionContainerComponent/Over18Component/index.js
+++ components/QuestionContainerComponent/Over18Component/index.js
@@ -5,7 +5,7 @@ import styles from './Over18Component.module.css'
  const Over18Component =({nextPage, isPrevEnabled, isDoneEnabled}) => {
   
   const [isOver18, setIsOver18] = useState(true);
-
+  const [isOver18andProxy, setIsOver18andProxy] = useState(true);
   useEffect(() => {
     isPrevEnabled(true);
     isDoneEnabled(false);
@@ -17,6 +17,8 @@ import styles from './Over18Component.module.css'
       <p className="error" hidden={isOver18}>
       This service is recommended for patients age 18 and over. We recommend contacting your medical provider directly or call the YNHHS COVID Call Center at 1-888-ASK-YNHH for a clinical assessment.
         </p>
+        <p className="error" hidden={isOver18andProxy}>
+        For any patient under the age of 18, a parent, legal guardian or designated adult is required to be present at the time of the test.        </p>
         <fieldset className="radio_grp_set">
           <legend>
           Are you over the age of 18? 

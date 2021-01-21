799d74c7e1e74b381b781c823e46821d78a5d126
TerenceJDew <terence.dew@gmail.com>
2021-01-21 16:58:53 -0500

    Updated Links

--- pages/scheduling.js
+++ pages/scheduling.js
@@ -12,8 +12,8 @@ export default function Home({ link }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className='grid'>
-      <img src="/YNHHSLogo.png"></img>
+      <div className="grid">
+        <img src="/YNHHSLogo.png"></img>
       </div>
 
       <div className="scheduleContainer">
@@ -41,11 +41,12 @@ export default function Home({ link }) {
 
 Home.getInitialProps = async ({ query }) => {
   const { symptoms } = query;
-  let link = symptoms == true ? '' : 'https://mychart.ynhhs.org/MyChart-PRD/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=79929,82746&vt=2102&dept=201570001&view=plain&public=1';
-  
+  let link =
+    symptoms == true
+      ? 'https://mychart.ynhhs.org/MyChart-PRD/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=84157&vt=2228&dept=201570001&view=plain&public=1'
+      : 'https://mychart.ynhhs.org/MyChart-PRD/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=79929,82746&vt=2102&dept=201570001&view=plain&public=1';
 
   return {
     link
   };
 };
- 

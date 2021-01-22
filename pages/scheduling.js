f4c42fc6b6394a779d65240d9fac633b47275316
TerenceJDew <terence.dew@gmail.com>
2021-01-22 13:13:00 -0500

    scheduling redirect

--- pages/scheduling.js
+++ pages/scheduling.js
@@ -1,71 +1,74 @@
-import Head from 'next/head';
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+// import Head from 'next/head';
+// import { useEffect } from 'react';
+// import { useRouter } from 'next/router';
+import redirect from 'nextjs-redirect'
 
-export default function Home({ link, recc_date, second_dose }) {
-  const router = useRouter();
 
-  useEffect(() => {
-    if (link == '') router.push(`/`);
-  }, []);
+// export default function Home({ link, recc_date, second_dose }) {
+//   const router = useRouter();
 
-  return (
-    <>
-      <Head>
-        <title>COVID-19 Vaccine Scheduler</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+//   useEffect(() => {
+//     if (link == '') router.push(`/`);
+//   }, []);
 
-      <div className="grid">
-        <img src="/YNHHSLogo.png"></img>
-      </div>
+//   return (
+//     <>
+//       <Head>
+//         <title>COVID-19 Vaccine Scheduler</title>
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
 
-      <div className="scheduleContainer">
-        <h3>
-          {second_dose == 'true' && `Please Select Date After ${recc_date}`}
-        </h3>
-        <h3>
-          {second_dose == 'false' && `Please Select Date After ${recc_date}`}
-        </h3>
-        <iframe
-          id="openSchedulingFrame"
-          className="widgetframe"
-          scrolling="yes"
-          src={link}
-        ></iframe>
-      </div>
-      <style jsx>{`
-        .scheduleContainer,
-        iframe {
-          width: 100%;
-          height: 100vh;
-          display: flex;
-          flex-direction: column;
-          justify-items: center;
-          border: none;
-        }
-        h3 {
-          margin-left: 25px;
-        }
-      `}</style>
-    </>
-  );
-}
+//       <div className="grid">
+//         <img src="/YNHHSLogo.png"></img>
+//       </div>
 
-Home.getInitialProps = async ({ query }) => {
-  const { recc_date, second_dose, isPfizer } = query;
-  let link =
-    second_dose == 'true'
-      ? isPfizer == 'true'
-        ? 'https://openscheduling.ynhhs.org/mychart-prd/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=83668,83667,74622,84623,83665,83652,83656,84799,84798,84796,84797,84800,84816&vt=2339&dept=204150016,204590014,201280003,208040011,204010005,204400009,102360001,102350001,102390001,102370001,102340001,102380001&view=plain&public=1'
-        :  'https://openscheduling.ynhhs.org/mychart-prd/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=83667,83868,83874,83664,83653,83655,84793,84794,84791,84792,84795,84815&vt=2338&dept=204150016,204590014,201280003,208040011,204010005,204400009,102360001,102350001,102390001,102370001,102340001,102380001&view=plain&public=1'
-      : 'https://openscheduling.ynhhs.org/mychart-prd/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=83666,83867,83873,83663,83651,83654,84788,84789,84786,84787,84790,84814&vt=2293&dept=204150016,204590014,201280003,208040011,204010005,204400009,102360001,102350001,102390001,102370001,102340001,102380001&view=plain&public=1';
+//       <div className="scheduleContainer">
+//         <h3>
+//           {second_dose == 'true' && `Please Select Date After ${recc_date}`}
+//         </h3>
+//         <h3>
+//           {second_dose == 'false' && `Please Select Date After ${recc_date}`}
+//         </h3>
+//         <iframe
+//           id="openSchedulingFrame"
+//           className="widgetframe"
+//           scrolling="yes"
+//           src={link}
+//         ></iframe>
+//       </div>
+//       <style jsx>{`
+//         .scheduleContainer,
+//         iframe {
+//           width: 100%;
+//           height: 100vh;
+//           display: flex;
+//           flex-direction: column;
+//           justify-items: center;
+//           border: none;
+//         }
+//         h3 {
+//           margin-left: 25px;
+//         }
+//       `}</style>
+//     </>
+//   );
+// }
 
-      if(second_dose == null) link = '';
+// Home.getInitialProps = async ({ query }) => {
+//   const { recc_date, second_dose, isPfizer } = query;
+//   let link =
+//     second_dose == 'true'
+//       ? isPfizer == 'true'
+//         ? 'https://openscheduling.ynhhs.org/mychart-prd/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=83668,83667,74622,84623,83665,83652,83656,84799,84798,84796,84797,84800,84816&vt=2339&dept=204150016,204590014,201280003,208040011,204010005,204400009,102360001,102350001,102390001,102370001,102340001,102380001&view=plain&public=1'
+//         :  'https://openscheduling.ynhhs.org/mychart-prd/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=83667,83868,83874,83664,83653,83655,84793,84794,84791,84792,84795,84815&vt=2338&dept=204150016,204590014,201280003,208040011,204010005,204400009,102360001,102350001,102390001,102370001,102340001,102380001&view=plain&public=1'
+//       : 'https://openscheduling.ynhhs.org/mychart-prd/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=83666,83867,83873,83663,83651,83654,84788,84789,84786,84787,84790,84814&vt=2293&dept=204150016,204590014,201280003,208040011,204010005,204400009,102360001,102350001,102390001,102370001,102340001,102380001&view=plain&public=1';
 
-  return {
-    link,
-    recc_date,
-    second_dose
-  };
-};
+//       if(second_dose == null) link = '';
+
+//   return {
+//     link,
+//     recc_date,
+//     second_dose
+//   };
+// };
+export default redirect('http://www.ynhhs.org/covidvaccine');

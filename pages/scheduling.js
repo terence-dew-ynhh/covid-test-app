b3317475ccdfadb79bb9bc6fb6448591137ff88e
TerenceJDew <terence.dew@gmail.com>
2021-01-27 10:07:50 -0500

    link update

--- pages/scheduling.js
+++ pages/scheduling.js
@@ -43,7 +43,7 @@ Home.getInitialProps = async ({ query }) => {
 
   let link =
     hasSymptoms === 'true'
-      ? 'https://mychart.ynhhs.org/mychart-prd/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=76698,76703,76701,81079,82383,76700,81452,76702,82948,82949&vt=2231&dept=103010061,104010062,108010035,103720003,101050022,102010045,100001365,108710023,100001382,100001383,103010119,104010098,108010101,102010096,108710074&view=plain&public=1'
+      ? 'https://mychart.ynhhs.org/mychart-prd/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=76698,76703,76701,81079,82383,76700,81452,76702,82948,82949,84072&vt=2231&dept=103010061,104010062,108010035,103720003,101050022,102010045,100001365,108710023,100001382,100001383,103010119,104010098,108010101,102010096,108710074,101980001&view=plain&public=1'
       : 'https://mychart.ynhhs.org/mychart-prd/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=83258,83259&vt=2102&dept=101500001,101460003&view=plain&public=1';
 
   return {

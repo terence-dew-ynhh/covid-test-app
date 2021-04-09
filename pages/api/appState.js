b5bd019bb974635787b3ee7f534dd34aa93024c4
TerenceJDew <terence.dew@gmail.com>
2021-04-09 09:34:12 -0400

    Updated api endpoint

--- pages/api/appState.js
+++ pages/api/appState.js
@@ -1,6 +1,6 @@
-import middleware from "../../middlewares/database";
-import nextConnect from "next-connect";
-import Cors from 'cors'
+import middleware from '../../middlewares/database';
+import nextConnect from 'next-connect';
+import Cors from 'cors';
 
 const handler = nextConnect();
 
@@ -8,68 +8,58 @@ handler.use(middleware);
 
 // Initializing the cors middleware
 const cors = Cors({
-  methods: ['GET','HEAD','POST'],
-})
+  methods: ['GET', 'HEAD', 'POST']
+});
 
 // Helper method to wait for a middleware to execute before continuing
 // And to throw an error when an error happens in a middleware
 function runMiddleware(req, res, fn) {
   return new Promise((resolve, reject) => {
-    fn(req, res, result => {
+    fn(req, res, (result) => {
       if (result instanceof Error) {
-        return reject(result)
+        return reject(result);
       }
 
-      return resolve(result)
-    })
-  })
+      return resolve(result);
+    });
+  });
 }
 
 handler.post(async (req, res) => {
-
-  await runMiddleware(req, res, cors)  
+  await runMiddleware(req, res, cors);
 
   let { openFlag } = req.body;
 
+  let isOpen = false;
 
-try{
-  const appStateFlag = await req.db
-    .collection('main_appstate')
-    .findOneAndUpdate({},{ $set: {open: openFlag}},{upsert: true});
-  
-
-   res.status(200);
-    res.send(appStateFlag);
-  } catch (err) {
-    res.status(400);
-    res.end(`Something went wrong: ${err}`);
+  if (openFlag == true) {
+    isOpen = true;
   }
 
+  try {
+    const appStateFlag = await req.db
+      .collection('main_appstate')
+      .findOneAndUpdate({}, { $set: { open: isOpen } }, { upsert: true });
+
+    res.status(200).send(appStateFlag);
+  } catch (err) {
+    res.status(400).end(`Error: ${err}`);
+  }
 });
-
-
 
 handler.get(async (req, res) => {
+  await runMiddleware(req, res, cors);
 
-  await runMiddleware(req, res, cors)  
+  try {
+    const appStateFlag = await req.db.collection('main_appstate').findOne({});
+    console.log(appStateFlag);
 
-
-try{
-  const appStateFlag = await req.db
-    .collection('main_appstate')
-    .findOne({})
-  console.log(appStateFlag)
-
-  
-
-   res.status(200);
+    res.status(200);
     res.send(appStateFlag);
   } catch (err) {
     res.status(400);
-    res.end(`Something went wrong: ${err}`);
+    res.end(`Error: ${err}`);
   }
-
 });
-
 
 export default handler;

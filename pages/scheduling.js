f874ada60dcd079268933e11262bcabd277c8bf8
TerenceJDew <terence.dew@gmail.com>
2021-05-25 15:16:41 -0400

    Updated Links

--- pages/scheduling.js
+++ pages/scheduling.js
@@ -12,8 +12,8 @@ export default function Home({ link }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className='grid'>
-      <img src="/YNHHSLogo.png"></img>
+      <div className="grid">
+        <img src="/YNHHSLogo.png"></img>
       </div>
 
       <div className="scheduleContainer">
@@ -41,23 +41,23 @@ export default function Home({ link }) {
 
 Home.getInitialProps = async ({ query }) => {
   const { endpoint } = query;
-  let link = '';
-  const locationMapping = [    
-    {
-      name: 'Mohegan Sun Employees',
-      link:
-        'https://mychart.ynhhs.org/MyChart-PRD/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=76698,76701,81079,82383,81452,76702,82948,82949,79643&vt=2228&dept=103010119,108010101,103720003,101050022,100001365,108710074,100001383,100001358,100001382&view=plain&public=1'
-    }
-  ];
+  let link =
+    'https://mychart.ynhhs.org/MyChart-PRD/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=76698,76701,81079,82383,81452,76702,82948,82949,79643&vt=2228&dept=103010119,108010101,103720003,101050022,100001365,108710074,100001383,100001358,100001382&view=plain&public=1';
+  // const locationMapping = [
+  //   {
+  //     name: 'Mohegan Sun Employees',
+  //     link:
+  //       'https://mychart.ynhhs.org/MyChart-PRD/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=76698,76701,81079,82383,81452,76702,82948,82949,79643&vt=2228&dept=103010119,108010101,103720003,101050022,100001365,108710074,100001383,100001358,100001382&view=plain&public=1'
+  //   }
+  // ];
 
-  locationMapping.forEach((element) => {
-    if (endpoint === element.name) {
-      link = element.link;
-    }
-  });
+  // locationMapping.forEach((element) => {
+  //   if (endpoint === element.name) {
+  //     link = element.link;
+  //   }
+  // });
 
   return {
     link
   };
 };
- 

49cc749ad83070bfcbc7dc9178575ef4219025a9
TerenceJDew <terence.dew@gmail.com>
2021-06-18 08:25:00 -0400

    url updates

--- pages/scheduling.js
+++ pages/scheduling.js
@@ -154,16 +154,16 @@ Home.getInitialProps = async ({ query }) => {
   let link = '';
 
   link =
-    'https://openscheduling.ynhhs.org/mychart-prd/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=87700&vt=2293&dept=204680001&view=plain&public=1';
+    'https://openscheduling.ynhhs.org/mychart-prd/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=87700,88037,88038,88040,88042,88043,88046&vt=2293&dept=204680001,204590014,203260005,204400009,201120002,204530003,208040011&view=plain&public=1';
 
   if (jjapproved == 'true') {
     link =
-      'https://openscheduling.ynhhs.org/mychart-prd/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=87702,87700&vt=2293&dept=204680001&view=plain&public=1';
+      'https://openscheduling.ynhhs.org/mychart-prd/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=87702,87700,88037,88038,88040,88042,88043,88046&vt=2293&dept=204680001,204590014,203260005,204400009,201120002,204530003,208040011&view=plain&public=1';
   }
 
   if (isOver18 == 'false') {
     link =
-      'https://openscheduling.ynhhs.org/mychart-prd/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=87700&vt=2293&dept=204680001&view=plain&public=1';
+      'https://openscheduling.ynhhs.org/mychart-prd/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=87700,88037,88038,88040,88042,88043,88046&vt=2293&dept=204680001,204590014,203260005,204400009,201120002,204530003,208040011&view=plain&public=1';
   }
 
   if (second_dose == 'true') {
@@ -171,7 +171,7 @@ Home.getInitialProps = async ({ query }) => {
       'https://openscheduling.ynhhs.org/mychart-prd/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=84792,84795,84794,94791,95375,84815,84793,85108&vt=2338&dept=101960001,102340001,102350001,102360001,102370001,102380001,102390001,102400001&view=plain&public=1';
     if (isPfizer == 'true') {
       link =
-        'https://openscheduling.ynhhs.org/mychart-prd/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=87701,84797,84800,84799,84796,85376,84816,84798,85109&vt=2339&dept=204680001,101960001,102340001,102350001,102360001,102370001,102380001,102390001,102400001&view=plain&public=1';
+        'https://openscheduling.ynhhs.org/mychart-prd/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=87701,88037,88038,88040,88042,88043,88046&vt=2339&dept=204680001,204590014,203260005,204400009,201120002,204530003,208040011&view=plain&public=1';
     }
   }
 

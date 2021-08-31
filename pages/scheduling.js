421777fb60e401c441207e0789809801167bff0f
TerenceJDew <terence.dew@gmail.com>
2021-08-31 15:37:56 -0400

    Updated url

--- pages/scheduling.js
+++ pages/scheduling.js
@@ -1,64 +1,14 @@
 import Head from 'next/head';
-import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import { makeStyles } from '@material-ui/core/styles';
-import IconButton from '@material-ui/core/IconButton';
-import ClearIcon from '@material-ui/icons/Clear';
 
-function getModalStyle() {
-  const top = 50;
-  const left = 52;
-  const innerLeft = 50;
-
-  return {
-    top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate(-${top}%, -${innerLeft}%)`
-  };
-}
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    position: 'absolute',
-    height: '90%',
-    backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-    outline: 0
-  }
-}));
-
-export default function Home({
-  link,
-  recc_date,
-  second_dose,
-  isSpanish,
-  isRiskGroup,
-  isOver18
-}) {
-  const [open, setOpen] = useState(false);
-  const [modalStyle] = useState(getModalStyle);
-
+export default function Home({ link }) {
   const router = useRouter();
-  const classes = useStyles();
-
-  useEffect(() => {
-    if (link == '') router.push(`/`);
-  }, []);
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
+  const { endpoint } = router.query;
+  console.log(endpoint);
   return (
     <>
       <Head>
-        <title>UNH COVID19 Vaccination</title>
+        <title>YNHH COVID-19 Test Scheduler</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -67,21 +17,12 @@ export default function Home({
       </div>
 
       <div className="scheduleContainer">
-        <h3>
-          {second_dose == 'true'
-            ? isSpanish == 'true'
-              ? `Seleccione una fecha posterior a ${recc_date}`
-              : `Please Select Date After ${recc_date}`
-            : ''}
-        </h3>
-
         <iframe
           id="openSchedulingFrame"
           className="widgetframe"
           scrolling="yes"
           src={link}
         ></iframe>
- 
       </div>
       <style jsx>{`
         .scheduleContainer,
@@ -89,70 +30,23 @@ export default function Home({
           width: 100%;
           height: 100vh;
           display: flex;
-          flex-direction: column;
+          align-items: center;
           justify-items: center;
           border: none;
         }
-        button {
-          width: 15%;
-        }
-        h3 {
-          margin-left: 25px;
-        }
-        @media (max-width: 475px) {
-          button {
-            width: 100%;
-          }
       `}</style>
     </>
   );
 }
 
 Home.getInitialProps = async ({ query }) => {
-  const {
-    recc_date,
-    second_dose,
-    isPfizer,
-    isSpanish,
-    isRiskGroup,
-    isOver18,
-    jjapproved
-  } = query;
-  let link = '';
-
-  link =
-    'https://openscheduling.ynhhs.org/mychart-prd/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=88976&vt=2443&dept=201570002&view=plain&public=1&lang=english';
-
-  // if (jjapproved == 'true') {
-  //   link =
-  //     'https://openscheduling.ynhhs.org/mychart-prd/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=88976,88977,88978&vt=2293&dept=201570002&view=plain&public=1';
-  // }
-
-  // if (isOver18 == 'false') {
-  //   link =
-  //     'https://openscheduling.ynhhs.org/mychart-prd/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=88976,88977,88978&vt=2293&dept=201570002&view=plain&public=1';
-  // }
-
-  // if (second_dose == 'true') {
-  //   link =
-  //     'https://openscheduling.ynhhs.org/mychart-prd/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=88977&vt=2338&dept=201570002&view=plain&public=1';
-  //   if (isPfizer == 'true') {
-  //     link =
-  //       'https://openscheduling.ynhhs.org/mychart-prd/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=88976,88977,88978&vt=2293&dept=201570002&view=plain&public=1';
-  //   }
-  // }
-
-  if (isSpanish == 'true') link = link + '&lang=espanol';
-  else link = link + '&lang=english';
-  if (second_dose == null) link = '';
+  const { symptoms } = query;
+  let link =
+    symptoms == "true"
+      ? 'https://openscheduling.ynhhs.org/mychart-prd/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=89130&vt=10220&dept=201570002&view=plain&public=1&lang=english'
+      : 'https://openscheduling.ynhhs.org/mychart-prd/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=89130&vt=10220&dept=201570002&view=plain&public=1&lang=english';
 
   return {
-    link,
-    recc_date,
-    second_dose,
-    isSpanish,
-    isRiskGroup,
-    isOver18
+    link
   };
 };
-// export default redirect('http://www.ynhhs.org/covidvaccine');

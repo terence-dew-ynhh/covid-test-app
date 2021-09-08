8558884fda2ed8932c320a69e69b696f0abeac63
TerenceJDew <terence.dew@gmail.com>
2021-09-08 17:22:13 -0400

    test link

--- pages/scheduling.js
+++ pages/scheduling.js
@@ -36,7 +36,7 @@ export default function Home({ link }) {
 }
 
 Home.getInitialProps = async () => {
-  let link = 'https://mychart.ynhhs.org/MyChart-PRD/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=82100&vt=10220&dept=204620001&view=plain&public=1';
+  let link = 'https://mychart.ynhhs.org/MyChart-PRD/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=79084,81416,79083,82948,81079,88751,83688,83687,81420,79081,81417,81418,81452,79085,82383,79080,79073,79078,79077&vt=2275&dept=101570001,101500001,101460003,100001382,103720003,101130001,101090001,101380001,101630001,101160001,101600001,101140001,100001365,101050004,101310002,101050022,101700005,103010045,104290002,104240001&view=plain&public=1';
 
   return {
     link

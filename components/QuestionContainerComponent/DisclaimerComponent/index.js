81f314db4be0928dbab0b424a385ac52f8114646
ismailcampbell <ismail.campbell@ynhh.org>
2022-01-19 17:08:11 -0500

    Added a new bullet point to Disclaimer Component

--- components/QuestionContainerComponent/DisclaimerComponent/index.js
+++ components/QuestionContainerComponent/DisclaimerComponent/index.js
@@ -18,6 +18,7 @@ import styles from './DisclaimerComponent.module.css'
         <b>• Appointment required at all locations. </b><br></br><br></br>
         <b>• Hospital Emergency Departments do not provide COVID screening tests. If you need a COVID screening test, please schedule through one of our sites.</b><br></br><br></br>
         <b>• Due to current demands for testing, results are typically available within 2 days.</b><br></br><br></br>
+        <b>• Repeat PCR testing is NOT generally indicated within 90 days of a positive test, either antigen or PCR, especially among patients with symptoms of COVID-19. For more information, <a target="__blank" href="https://www.ynhhs.org/patient-care/covid-19/testing/testing-locations">CLICK HERE </a></b><br></br><br></br>
         <b>• Results may not be available for travel documentation.</b><br></br><br></br>
         </p>
         <fieldset className="radio_grp_set">

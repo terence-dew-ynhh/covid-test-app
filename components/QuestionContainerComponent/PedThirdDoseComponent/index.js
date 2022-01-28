356fb537d953c67210fd240c08f24ce780939c23
TerenceJDew <terence.dew@gmail.com>
2022-01-28 13:03:46 -0500

    Removed Red copy  PedThirdDose

--- components/QuestionContainerComponent/PedThirdDoseComponent/index.js
+++ components/QuestionContainerComponent/PedThirdDoseComponent/index.js
@@ -37,7 +37,7 @@ const PedThirdDoseComponent = ({
         </p>
         <fieldset className="radio_grp_set">
           <legend>
-            <b>
+          <b>
               I attest to having one or more of the following immune
               compromising conditions:
             </b>
@@ -55,17 +55,18 @@ const PedThirdDoseComponent = ({
             <br></br>- Have received a stem cell transplant or CAR T-cell
             therapy
             <br></br>
-            <br></br>- I have received a solid organ transplant (such as a
+            <br></br>- Have received a solid organ transplant (such as a
             kidney or liver transplant) and am taking immune-suppressing
             medication
             <br></br>
-            <br></br>- I have a moderate or severe primary immunodeficiency
+            <br></br>- Have a moderate or severe primary immunodeficiency
             (such as DiGeorge or Wiskott-Aldrich syndrome)
             <br></br>
-            <br></br>- Advanced or untreated HIV disease <br></br>
+            <br></br>- Have a Advanced or untreated HIV disease <br></br>
             <br></br>- I am taking (or was taking at the time of my initial mRNA
             COVID-19 vaccine series) high-dose corticosteroids (at least 20 mg
-            of prednisone daily) or any of the other medications            
+            of prednisone daily) or any of the other medications on this list
+            that suppress the immune system
             {' '}
             <a
               target="__blank"
@@ -101,7 +102,6 @@ const PedThirdDoseComponent = ({
         </fieldset>
         <br></br>
         <br></br>
-        <b className="redText">You may still be eligible for a booster dose</b>
       </div>
       <style jsx>{``}</style>
     </>

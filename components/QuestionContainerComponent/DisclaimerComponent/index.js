b7f22d35217ae51435badbc35f70c59af80ad414
TerenceJDew <terence.dew@gmail.com>
2022-02-04 09:26:50 -0500

    Updated Disclaimer Copy

--- components/QuestionContainerComponent/DisclaimerComponent/index.js
+++ components/QuestionContainerComponent/DisclaimerComponent/index.js
@@ -15,7 +15,7 @@ import styles from './DisclaimerComponent.module.css'
     <>
       <div className={styles.question_row_item}>
       <p className="error">
-        <b>• Appointment required at all locations. </b><br></br><br></br>
+        <b>• Many sites require an appointment. </b><br></br><br></br>
         <b>• Hospital Emergency Departments do not provide COVID screening tests. If you need a COVID screening test, please schedule through one of our sites.</b><br></br><br></br>
         <b>• Repeat PCR testing is NOT generally indicated within 90 days of a positive test, either antigen or PCR, especially among patients with symptoms of COVID-19. For more information, <a target="__blank" href="https://www.ynhhs.org/patient-care/covid-19/testing/testing-locations">CLICK HERE </a></b><br></br><br></br>
         <b>• Due to current demands for testing, results are typically available within 2 days.</b><br></br><br></br>

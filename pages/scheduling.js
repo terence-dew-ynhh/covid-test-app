c3db9ddfece206a6f5634634702e825aa28b3a86
TerenceDew <terence.dew@ynhh.org>
2022-09-16 13:57:08 -0400

    PRD link added

--- pages/scheduling.js
+++ pages/scheduling.js
@@ -7,7 +7,7 @@ export default function Home({ link }) {
       <Head>
         <title>Mohegan Flu Vaccine Scheduler</title>
         <link rel="icon" href="/favicon.ico" />
-      <script src="https://mychartnp.ynhhs.org/poc/Scripts/lib/Widget/widget_sdk.js"></script>
+      <script src="https://mychart.ynhhs.org/mychart-prd/Scripts/lib/Widget/widget_sdk.js"></script>
       </Head>
 
       <div className="grid">
@@ -15,13 +15,13 @@ export default function Home({ link }) {
       </div>
       <div className="scheduleContainer">
       <div id="open-scheduler"
-        data-url="https://MyChartnp.ynhhs.org/poc"
+        data-url="https://MyChart.ynhhs.org/mychart-prd"
         data-apikey="vXykxvjGIe80218"
         data-widget-type="openscheduling"
         data-additionalparams-dept="204620001"
         data-additionalparams-vt="10220"
         data-additionalparams-id="82100">
-          
+
         </div>
       </div>
       <style jsx>{`

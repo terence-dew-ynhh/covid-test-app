6943ae926fa08b7600a802b88e26dac25d8cbf93
TerenceDew <terence.dew@ynhh.org>
2022-09-16 16:14:08 -0400

    Testing domain access for scheduler

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,33 @@
-import React, { useState } from 'react';
 import Head from 'next/head';
-import QuestionContainerComponent from '../components/QuestionContainerComponent';
+import { useRouter } from 'next/router';
 
 export default function Home() {
   return (
-    <div className="container">
+    <>
       <Head>
         <title>Mohegan Flu Vaccine Scheduler</title>
         <link rel="icon" href="/favicon.ico" />
+        <script src="https://mychartnp.ynhhs.org/poc/Scripts/lib/Widget/widget_sdk.js"></script>
       </Head>
+
       <div className="grid">
         <img src="/YNHHSLogo.png"></img>
       </div>
-      <h1 className="title">
-      Mohegan Flu Vaccine Scheduler
-      </h1>
-      <QuestionContainerComponent></QuestionContainerComponent>
-    </div>
+      {/* <div className="scheduleContainer"> */}
+        <div id="open-scheduler"
+          data-url="https://myChartnp.ynhhs.org/poc"
+          data-apikey="vXykxvjGIe80218"
+          data-widget-type="openscheduling"
+          data-additionalparams-dept="204620001"
+          data-additionalparams-vt="10220"
+          data-additionalparams-id="82100">
+        </div>
+      {/* </div> */}
+      <style jsx>{`
+
+      `}</style>
+    </>
   );
 }
+
+

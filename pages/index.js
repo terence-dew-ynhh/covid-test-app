ad5231a89265e0f2f566867039b4af34d90c8431
TerenceDew <terence.dew@ynhh.org>
2022-09-16 16:04:05 -0400

    Styling Fix and revert

--- pages/index.js
+++ pages/index.js
@@ -5,37 +5,17 @@ import QuestionContainerComponent from '../components/QuestionContainerComponent
 export default function Home() {
   return (
     <div className="container">
-        <>
       <Head>
         <title>Mohegan Flu Vaccine Scheduler</title>
         <link rel="icon" href="/favicon.ico" />
-      <script src="https://mychart.ynhhs.org/mychart-prd/Scripts/lib/Widget/widget_sdk.js"></script>
       </Head>
-
       <div className="grid">
         <img src="/YNHHSLogo.png"></img>
       </div>
-      <div className="scheduleContainer">
-      <div id="open-scheduler"
-        data-url="https://MyChart.ynhhs.org/mychart-prd"
-        data-apikey="vXykxvjGIe80218"
-        data-widget-type="openscheduling"
-        data-additionalparams-dept="204620001"
-        data-additionalparams-vt="10220"
-        data-additionalparams-id="82100">
-        </div>
-      </div>
-      <style jsx>{`
-        .scheduleContainer{
-          width: 100%;
-          height: 100vh;
-          display: flex;
-          align-items: center;
-          justify-items: center;
-          border: none;
-        }
-      `}</style>
-    </>
+      <h1 className="title">
+      Mohegan Flu Vaccine Scheduler
+      </h1>
+      <QuestionContainerComponent></QuestionContainerComponent>
     </div>
   );
 }
--- pages/scheduling.js
+++ pages/scheduling.js
@@ -1,46 +1,33 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 
-export default function Home({ link }) {
+export default function Home() {
   return (
     <>
       <Head>
         <title>Mohegan Flu Vaccine Scheduler</title>
         <link rel="icon" href="/favicon.ico" />
-      <script src="https://mychart.ynhhs.org/mychart-prd/Scripts/lib/Widget/widget_sdk.js"></script>
+        <script src="https://mychart.ynhhs.org/mychart-prd/Scripts/lib/Widget/widget_sdk.js"></script>
       </Head>
 
       <div className="grid">
         <img src="/YNHHSLogo.png"></img>
       </div>
-      <div className="scheduleContainer">
-      <div id="open-scheduler"
-        data-url="https://MyChart.ynhhs.org/mychart-prd"
-        data-apikey="vXykxvjGIe80218"
-        data-widget-type="openscheduling"
-        data-additionalparams-dept="204620001"
-        data-additionalparams-vt="10220"
-        data-additionalparams-id="82100">
+      {/* <div className="scheduleContainer"> */}
+        <div id="open-scheduler"
+          data-url="https://MyChart.ynhhs.org/mychart-prd"
+          data-apikey="vXykxvjGIe80218"
+          data-widget-type="openscheduling"
+          data-additionalparams-dept="204620001"
+          data-additionalparams-vt="10220"
+          data-additionalparams-id="82100">
         </div>
-      </div>
+      {/* </div> */}
       <style jsx>{`
-        .scheduleContainer{
-          width: 100%;
-          height: 100vh;
-          display: flex;
-          align-items: center;
-          justify-items: center;
-          border: none;
-        }
+
       `}</style>
     </>
   );
 }
 
-Home.getInitialProps = async () => {
-  let link = 'https://mychart.ynhhs.org/MyChart-PRD/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=82100&vt=10220&dept=204620001&view=plain&public=1';
 
-  return {
-    link
-  };
-};

7471282ef2fe9236bda4cff92766b6265b88131d
TerenceDew <terence.dew@ynhh.org>
2022-09-19 15:12:32 -0400

    fix Import

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import './globals/styles.css'
+import Script from 'next/script'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {

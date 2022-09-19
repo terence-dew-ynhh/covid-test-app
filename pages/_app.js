cd8f7371af291f7950668eb2aef18b9259e59696
TerenceDew <terence.dew@ynhh.org>
2022-09-19 15:11:34 -0400

    Trying Script after load

--- pages/_app.js
+++ pages/_app.js
@@ -2,5 +2,11 @@ import './globals/styles.css'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Script
+      src="https://mychart.ynhhs.org/mychart-prd/Scripts/lib/Widget/widget_sdk.js"
+      strategy="afterInteractive"
+    ></Script>
+    <Component {...pageProps} />
+  </>
 }

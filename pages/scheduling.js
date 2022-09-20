dd10df7179cd51c9c1d7d30ca58b4859989cec69
TerenceDew <terence.dew@ynhh.org>
2022-09-20 10:19:31 -0400

    Updated Scheduler

--- pages/scheduling.js
+++ pages/scheduling.js
@@ -1,7 +1,6 @@
 import Head from 'next/head';
-import Script from 'next/script'
 
-export default function Home() {
+export default function Home({ link }) {
   return (
     <>
       <Head>
@@ -12,21 +11,33 @@ export default function Home() {
       <div className="grid">
         <img src="/YNHHSLogo.png"></img>
       </div>
-      {/* <div className="scheduleContainer"> */}
-      <div id="open-scheduler"
-        data-url="https://MyChart.ynhhs.org/mychart-prd"
-        data-apikey="vXykxvjGIe80218"
-        data-widget-type="openscheduling"
-        data-additionalparams-dept="204620001"
-        data-additionalparams-vt="10220"
-        data-additionalparams-id="82100">
+      <div className="scheduleContainer">
+        <iframe
+          id="openSchedulingFrame"
+          className="widgetframe"
+          scrolling="yes"
+          src={link}
+        ></iframe>
       </div>
-      {/* </div> */}
       <style jsx>{`
-
-`}</style>
+        .scheduleContainer,
+        iframe {
+          width: 100%;
+          height: 100vh;
+          display: flex;
+          align-items: center;
+          justify-items: center;
+          border: none;
+        }
+      `}</style>
     </>
   );
 }
 
+Home.getInitialProps = async () => {
+  let link = 'https://mychart.ynhhs.org/MyChart-PRD/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=82100&vt=10220&dept=204620001&view=plain&public=1';
 
+  return {
+    link
+  };
+};

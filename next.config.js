0e0c095cc3a578e9b104970479c17381648eda1d
TerenceDew <terence.dew@ynhh.org>
2022-10-11 14:37:39 -0400

    Updated links and config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,4 @@
-require('dotenv').config();
 
 module.exports = {
-  publicRuntimeConfig: {
-    NODE_ENV: process.env.NODE_ENV,
-    PORT: process.env.PORT || 3015
-  },
-  devIndicators: {
-    autoPrerender: false,
-  },
-  target: 'serverless',
-  webpack: (config) => {
-    config.node = {
-      fs: 'empty'
-    }
-    return config
-  }
+
 };

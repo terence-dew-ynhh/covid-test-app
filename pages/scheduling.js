bdb836ff92e2306106345e601d23212966328da3
TerenceDew <terence.dew@ynhh.org>
2022-12-04 10:51:59 -0500

    Epic upgrade fix

--- pages/scheduling.js
+++ pages/scheduling.js
@@ -1,10 +1,14 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-
+import { useEffect } from 'react';
 export default function Home({ link }) {
   const router = useRouter();
   const { endpoint } = router.query;
-  console.log(endpoint);
+  
+  useEffect(() => {
+    window.open(link, '_blank').focus();
+  }, []);
+
   return (
     <>
       <Head>
@@ -43,8 +47,8 @@ Home.getInitialProps = async ({ query }) => {
   const { symptoms } = query;
   let link =
     symptoms == "true"
-      ? 'https://openscheduling.ynhhs.org/mychart-prd/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=88976&vt=2443&dept=201570002&view=plain&public=1&lang=english'
-      : 'https://openscheduling.ynhhs.org/mychart-prd/openscheduling/SignupAndSchedule/EmbeddedSchedule?id=88976&vt=2443&dept=201570002&view=plain&public=1&lang=english';
+      ? 'https://openscheduling.ynhhs.org/mychart-prd/openscheduling/standalone?id=88976&vt=2443&dept=201570002&view=plain&public=1&lang=english'
+      : 'https://openscheduling.ynhhs.org/mychart-prd/openscheduling/standalone?id=88976&vt=2443&dept=201570002&view=plain&public=1&lang=english';
 
   return {
     link

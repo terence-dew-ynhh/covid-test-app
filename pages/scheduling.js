5b47e6befba0b1be336cb098bb42dbb361b8aa5d
TerenceDew <terence.dew@ynhh.org>
2023-01-17 09:27:18 -0500

    Scheduling link opens in same tab - Apple device fix

--- pages/scheduling.js
+++ pages/scheduling.js
@@ -7,7 +7,7 @@ export default function Home({ link }) {
   const { endpoint } = router.query;
 
   useEffect(() => {
-    window.open(link, '_blank').focus();
+    window.open(link).focus();
   }, []);
 
   return (
